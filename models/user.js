ab6c90626712c576fdabb0453db4880765317ec6
boburastonov <boburastonov33@gmail.com>
2026-02-23 15:16:43 +0500

    Add user js

---
+++ models/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: Number,
+  email: {
+    type: String,
+    unique: true,
+  },
+});
+
+export default mongoose.model("User", userSchema);
